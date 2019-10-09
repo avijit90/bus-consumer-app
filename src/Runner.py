286bc08ab3c4b80e1e14a1de6563127ce36cb0ef
Avijit Vaidya <avijit.vaidya@finastra.com>
2019-10-09 18:03:46 +0800

    Config updated to specify topicName and added Es connection check status.

--- src/Runner.py
+++ src/Runner.py
@@ -2,13 +2,18 @@ import logging
 
 from Consumer import Consumer
 from ElasticConnector import ElasticConnector
-from src.Config import es_url, kafka_server_url
+from src.Config import es_url, kafka_server_url, topic_name
+
+
+def get_es_connection_status():
+    return 'Success' if es_client.test_connection() else 'Failure'
+
 
 es_client = ElasticConnector(es_url)
 es_client.create_es_connector()
-print(f'connection successful : {es_client.test_connection()}')
+print(f'ES connection status : {get_es_connection_status()}')
 
-consumer = Consumer(kafka_server_url, es_client.es_connector)
+consumer = Consumer(kafka_server_url, es_client.es_connector, topic_name)
 consumer.create_kafka_client()
 consumer.consume_messages()
 
