9710be5da423241e09ad82f3e6bf5a93002f58e8
Avijit Vaidya <avijit.vaidya@finastra.com>
2019-10-09 17:25:07 +0800

    ES Connector refactored.

--- src/elastic/ElasticConnector.py
+++ src/elastic/ElasticConnector.py
@@ -4,20 +4,19 @@ import re
 import certifi
 from elasticsearch import Elasticsearch
 
-from ESConnectionException import ESConnectionException
-
 
 class ElasticConnector:
 
-    def __init__(self):
-        self.es_connector = None
+    def __init__(self, elastic_url):
+        self.elastic_url = elastic_url
+        self.es_connector = None,
+        self.es_config = None
 
-    @staticmethod
-    def get_elastic_configuration():
+    def create_es_config(self):
         # Parse the auth and host from env:
-        bonsai = 'https://ftdcqh6z2x:14zc0p3aji@bus-dashboard-cluste-8058502828.ap-southeast-2.bonsaisearch.net'
-        auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
-        host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+        auth = re.search('https\:\/\/(.*)\@', self.elastic_url).group(1).split(':')
+        host = self.elastic_url.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
         # Connect to cluster over SSL using auth for best security:
         es_config = [{
             'host': host,
@@ -26,20 +25,15 @@ class ElasticConnector:
             'http_auth': (auth[0], auth[1]),
             'ca_certs': certifi.where()
         }]
-        return es_config
+        self.es_config = es_config
+
+    def create_es_connector(self):
+        self.create_es_config()
+        es_connector = Elasticsearch(self.es_config)
+        self.es_connector = es_connector
 
     def test_connection(self):
-        if not self.es_connector.ping():
-            raise ESConnectionException
-
-        logging.info('Elastic search connection successful !')
-
-    def get_es_connector(self):
-        es_config = ElasticConnector.get_elastic_configuration()
-        es_connector = Elasticsearch(es_config)
-        self.test_connection()
-        self.es_connector = es_connector
-        return self.es_connector
+        return self.es_connector.ping()
 
 
 if __name__ == '__main__':
