06c7046a654a08ade4648cafb438843bdcf5e4c7
avijit90 <avijit90@users.noreply.github.com>
2026-05-22 02:43:38 +0000

    Fix local runner configuration

--- src/Config.py
+++ src/Config.py
@@ -1,3 +1,8 @@
 kafka_server_url = 'localhost:9092'
 es_url = 'https://ftdcqh6z2x:14zc0p3aji@bus-dashboard-cluste-8058502828.ap-southeast-2.bonsaisearch.net'
 topic_name = 'hjhkjhkj'
+
+consumer_config = {
+    'bootstrap.servers': kafka_server_url,
+    'group.id': 'bus-consumer-app',
+}
